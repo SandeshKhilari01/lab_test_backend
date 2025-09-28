23d32d99a8a552f8e861116cdd8440c0493a382f
Ashitosh Katale <kataleashitosh123@gmail.com>
2025-09-28 17:05:30 +0530

    Added sender email 1

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -63,7 +63,7 @@ const emailConfig = {
   sgMail: sgMail,
   from: {
     name: process.env.EMAIL_FROM_NAME || 'Lab Test Confirmation',
-    address: "troupetech9@gmail.com"
+    address: process.env.EMAIL_USER
   }
 };
 
